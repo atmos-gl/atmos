07af61f3a67babb60efa86e7765a3c6fe73e9c48
Léo Boyer <contact@leoboyer.fr>
2022-05-03 13:58:35 +0200

    add vue router

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import 'virtual:windi.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
