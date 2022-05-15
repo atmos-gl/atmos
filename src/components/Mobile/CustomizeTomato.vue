e1fe99dddfbc25ddc99d6e465f16624968e834aa
Léo Boyer <contact@leoboyer.fr>
2022-05-15 15:39:49 +0200

    CustomizeTomato.vue UI tweak

--- src/components/Mobile/CustomizeTomato.vue
+++ src/components/Mobile/CustomizeTomato.vue
@@ -4,6 +4,7 @@ import {reactive} from 'vue';
 import useLoader from '../../composables/useLoader';
 import tomatoResources from '../../three/resources/tomatoResources';
 import ColorChooser from './ColorChooser.vue';
+import {animate} from 'popmotion';
 
 const tomato = reactive({
   long: 1,
@@ -11,16 +12,31 @@ const tomato = reactive({
   color: 'red'
 })
 const { loading } = useLoader(tomatoResources)
+
+const resetTomato = () => {
+  tomato.color = 'red'
+  animate({
+    from: tomato,
+    to: {
+      long: 1,
+      size: 1,
+      color: 'red'
+    },
+    onUpdate: v => {
+      tomato.long = v.long
+      tomato.size = v.size
+    }
+  })
+}
 </script>
 <template>
-  <div class="h-full w-full flex flex-col items-center gap-4">
-    <div class="flex flex-col flex-grow items-center gap-4 pt-10 w-full relative">
-      <h1 class="font-title text-2xl px-10 relative z-10">Personnaliser votre tomate</h1>
-      <div class="w-full h-14/10 -my-2/10 text-white relative">
-        <TomatoScene v-if="!loading" :tomato="tomato" />
-      </div>
+  <div class="min-h-full w-full relative flex flex-col">
+    <h1 class="fixed top-0 left-0 w-full font-title text-2xl pt-10 text-center px-10 z-10">Personnaliser votre tomate</h1>
+    <div class="fixed top-16 left-0 w-full h-[60vh] -mt-2/10 -mb-4/10 text-white">
+      <TomatoScene v-if="!loading" :tomato="tomato" />
     </div>
-    <div class="flex flex-col w-full gap-4 overflow-y-scroll px-10 pb-10 relative z-15">
+    <div class="h-[50vh] pointer-events-none"></div>
+    <div class="flex flex-col w-full gap-3 px-10 pb-10 relative z-15">
       <div class="font-light w-full text-jade">Forme</div>
       <input type="range"
              class="input-range w-full  my-1"
@@ -33,12 +49,12 @@ const { loading } = useLoader(tomatoResources)
         <span>Plate</span>
         <span>Allongée</span>
       </div>
-      <div class="font-light w-full text-jade mt-2">Taille</div>
+      <div class="font-light w-full text-jade mt-4">Taille</div>
       <input type="range"
              class="input-range w-full  my-1"
              v-model.number="tomato.size"
-             min="0.7"
-             max="1.5"
+             min="0.6"
+             max="1.4"
              step="0.01"
       />
       <div class="w-full flex justify-between text-xs font-light text-jade/50">
@@ -46,8 +62,12 @@ const { loading } = useLoader(tomatoResources)
         <span>Normale</span>
         <span>Grosse</span>
       </div>
-      <div class="font-light w-full text-jade mt-2">Couleur</div>
+      <div class="font-light w-full text-jade mt-4">Couleur</div>
       <ColorChooser v-model="tomato.color" />
+      <div class="flex justify-between mt-6">
+        <button @click="resetTomato">Réinitialiser</button>
+        <button>Valider</button>
+      </div>
     </div>
   </div>
 </template>
