1265cf0b58def11594b6c7a30fd84a725df0392c
Léo Boyer <contact@leoboyer.fr>
2022-05-15 19:57:43 +0200

    Grow steps animation

---
+++ src/components/Mobile/GrowTomatoes.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+import GrowSteps from './GrowSteps.vue';</script>
+<template>
+  <div class="min-h-full w-full relative flex flex-col p-10 gap-4">
+    <h1 class="font-title text-2xl text-center">C'est parti !</h1>
+    <GrowSteps />
+  </div>
+</template>
