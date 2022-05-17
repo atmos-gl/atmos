695627641b1af91cdb44267ce1134300348c572a
Johann <johanndumoulin734@gmail.com>
2022-05-17 11:31:03 +0200

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <footer class="px-4 sm:px-14 flex flex-col items-center pb-8 text-pearl">
+    <div class="w-full border-t border-b border-white flex flex-col items-center py-10 sm:pt-20 sm:pb-20 md:pb-40">
+      <img src="./../assets/img/logo.png" class="mb-8">
+      <p class="w-44 text-center">La serre qui réduit l'effet de serre</p>
+    </div>
+    <p class="mt-2 text-sm font-light text-center">Site créé dans le cadre d'un projet tutoré DMII1 Gobelins Annecy</p>
+  </footer>
+</template>
