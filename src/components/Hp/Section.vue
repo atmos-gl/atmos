389d35c995a4b72e04820dc58de7836078abeee5
Johann <johanndumoulin734@gmail.com>
2022-05-19 11:53:32 +0200

    Add png to homepage intro

--- src/components/Hp/Section.vue
+++ src/components/Hp/Section.vue
@@ -5,8 +5,12 @@ const props = defineProps(['data'])
 </script>
 
 <template>
-  <section class="o-container flex justify-between flex-col" :class="data.isReversed ? 'lg:flex-row-reverse' : 'lg:flex-row'">
-    <div class="w-full md:w-3/4 lg:w-1/2 snake"></div>
+  <section class="o-container flex justify-between items-center flex-col" :class="data.isReversed ? 'lg:flex-row-reverse' : 'lg:flex-row'">
+    <div class="w-full md:w-3/4 lg:w-1/2">
+      <div class="relative max-w-introModels mx-auto snake" style="height: 550px">
+        <img v-for="img in data.models" :src="img.src" :alt="img.alt" :class="img.classes">
+      </div>
+    </div>
 
     <div class="w-full md:w-3/4 lg:w-1/2" :class="data.isReversed ? 'lg:text-left' : 'lg:text-right'">
       <h2 class="font-title text-5xl mb-12 font-bold text-jade"
--- src/data/hpSectionsData.ts
+++ src/data/hpSectionsData.ts
@@ -2,25 +2,89 @@ const data = [
     {
         title: "Le monde entier dans votre assiette",
         text: "Vous en avez marre de manger toujours les mêmes choses ? Vous rêvez d’essayer les fruits et légumes du monde entier mais vous ne les trouverez jamais en magasins ? Grâce à la serre Atome faites pousser chez vous les fruits et légumes d’ailleurs. Transformer l’exotique en local, en réduisant l’impact de l’importation des fruits et légumes.",
-        img: "",
+        models: [
+            {
+                src: '/src/assets/img/intro/assiette.png',
+                alt: 'Assiette',
+                classes: 'absolute h-70 top-30 left-0'
+            },
+            {
+                src: '/src/assets/img/intro/chocolat.png',
+                alt: 'Chocolat',
+                classes: 'absolute h-40 top-30 right-0'
+            },
+            {
+                src: '/src/assets/img/intro/cafe.png',
+                alt: 'Graine de café',
+                classes: 'absolute h-20 bottom-30 right-20'
+            }
+        ],
         isReversed: false
     },
     {
         title: "Manger végan, sain et équilibré",
         text: "Vous êtes attachés à la cause animale ou vous voulez simplement réduire votre consommation de viande ? Passez à la serre. Notre marque est 100% cruelty Free et tous nos produits sont végan. Grâce à la serre Atome il vous sera beaucoup facile de manger 5 fruits et légumes par jour. Vous développerez de meilleures habitudes alimentaires.",
-        img: "",
+        models: [
+            {
+                src: '/src/assets/img/intro/couvert.png',
+                alt: 'Couverts',
+                classes: 'absolute w-50 top-0 left-0'
+            },
+            {
+                src: '/src/assets/img/intro/Avocat.png',
+                alt: 'Avocat',
+                classes: 'absolute w-25 bottom-30 right-15'
+            },
+            {
+                src: '/src/assets/img/intro/vegan.png',
+                alt: 'Vegan',
+                classes: 'absolute h-25 bottom-0 left-35'
+            }
+        ],
         isReversed: true
     },
     {
         title: "Faites tout pousser en un clin d’œil",
         text: "Grâce à notre technologie à base de nucléaires, les plantes bénéficient d’un bienfait révolutionnaire qui permet de les faire pousser 100 plus rapidement qu’une plantation classique. Et tout cela dans le respect des traditions de leurs pays d’origine. Nous avons développé toute une gamme de produits venant du monde entier afin de vous permettre de développer tout type de fruits et légumes.",
-        img: "",
+        models: [
+            {
+                src: '/src/assets/img/intro/100.png',
+                alt: '100',
+                classes: 'absolute h-20 top-15 left-20'
+            },
+            {
+                src: '/src/assets/img/intro/baguette.png',
+                alt: 'Baguette magique',
+                classes: 'absolute w-60 bottom-0 left-0'
+            },
+            {
+                src: '/src/assets/img/intro/ananas.png',
+                alt: 'Ananas',
+                classes: 'absolute w-40 bottom-0 right-0'
+            }
+        ],
         isReversed: false
     },
     {
         title: "Autonome et connecté",
         text: "Peur de la facture énergétique ? Plus besoin d’électricité pour alimenter votre serre. La serre est alimentée par un système à fission nucléaire grâce à une microscopique pilule d’uranium. Et cela pour plusieurs centaines d’années, il s’agit d’une énergie durable qui pourrait profiter à votre famille sur plusieurs décennies. De plus, vous pouvez agir sur votre serre à n’importe quel moment grâce à l’application connectée à celle-ci.",
-        img: "",
+        models: [
+            {
+                src: '/src/assets/img/intro/Cactus.png',
+                alt: 'Cactus',
+                classes: 'absolute w-60 bottom-0 left-40'
+            },
+            {
+                src: '/src/assets/img/intro/eclair.png',
+                alt: 'Eclair',
+                classes: 'absolute h-40 top-0 left-0'
+            },
+            {
+                src: '/src/assets/img/intro/pillule.png',
+                alt: 'Pillule',
+                classes: 'absolute h-25 top-20 right-0'
+            }
+        ],
         isReversed: true
     },
 
