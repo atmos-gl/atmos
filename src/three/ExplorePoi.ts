99865a3a15acc3a5aca15db5d498b2db3582082a
Johann <johanndumoulin734@gmail.com>
2022-05-19 16:54:47 +0200

    Add ambient light to explore

--- src/three/ExplorePoi.ts
+++ src/three/ExplorePoi.ts
@@ -32,6 +32,7 @@ export class ExplorePoi extends BaseScene {
     private secondGreenHouseInitialPos: Vector3;
     private secondPipe: Object3D;
     private secondPipeInitialPos: Vector3;
+    private ambientLight: AmbientLight;
 
     constructor(poiDesc: Ref<string>, showBgText: Ref<boolean>) {
         super();
@@ -43,8 +44,8 @@ export class ExplorePoi extends BaseScene {
     public init(canvas: HTMLCanvasElement) {
         super.init(canvas)
 
-        // this.ambientLight = new AmbientLight('#ffffff', 0)
-        // this.scene.add(this.ambientLight)
+        this.ambientLight = new AmbientLight('#ffffff', 0.2)
+        this.scene.add(this.ambientLight)
 
         this.pointLight = new PointLight('#fff', 0.9)
         this.pointLight.position.x = 2
