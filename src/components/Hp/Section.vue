3a7b91a05de7c24306afa30403bea8570c34ef87
Léo Boyer <contact@leoboyer.fr>
2022-05-29 21:42:49 +0200

    wip fail on sprites

--- src/components/Hp/Section.vue
+++ src/components/Hp/Section.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 
+import SpritePlayer from './SpritePlayer.vue';
+import ananas from '../../assets/img/sprites/ananas.png'
+
 const props = defineProps(['data'])
 
 </script>
@@ -8,7 +11,8 @@ const props = defineProps(['data'])
   <section class="o-container flex justify-between items-center flex-col py-35" :class="data.isReversed ? 'lg:flex-row-reverse' : 'lg:flex-row'">
     <div class="w-full md:w-3/4 lg:w-1/2">
       <div class="relative max-w-introModels mx-auto snake" style="height: 550px">
-        <img v-for="img in data.models" :src="img.src" :alt="img.alt" :class="img.classes">
+<!--        <img v-for="img in data.models" :src="img.src" :alt="img.alt" :class="img.classes">-->
+        <SpritePlayer :sprite="ananas" />
       </div>
     </div>
 
---
+++ src/components/Hp/SpritePlayer.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+import {onMounted, ref} from 'vue';
+import AnimateSprite from '@its2easy/animate-sprite';
+
+const props = defineProps<{ sprite: string }>()
+const {sprite} = props
+console.log(sprite)
+
+const element = ref(null)
+onMounted(() => {
+  const player = new AnimateSprite(element.value, {
+    width: 700,
+    height: 1024,
+    frames: 91,
+    fps: 60,
+    loop: false,
+    onAnimationEnd: () => {
+      console.log('hey')
+      player.setReverse(true)
+      player.play()
+    }
+  });
+  player.play()
+
+})
+
+</script>
+
+<template>
+  <div ref="element" :style="{ backgroundImage: `url(${sprite})` }" class="w-64"></div>
+</template>
+
+<style scoped>
+
+</style>
