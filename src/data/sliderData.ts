0995dee48f7da3020d32518bd0d0fc7b80856f6e
Johann <johanndumoulin734@gmail.com>
2022-06-07 13:16:29 +0200

    Failed attempt on slider library

---
+++ src/data/sliderData.ts
@@ -0,0 +1,367 @@
+import localisation from '../assets/img/localIcon.svg'
+import label from '../assets/img/naturalIcon.svg'
+import water_amazonie from '../assets/img/shop/water/bouteille_Amazonie.png'
+import water_ecolo from '../assets/img/shop/water/bouteille_Ecolo.png'
+import water_Hymalaya from '../assets/img/shop/water/Bouteille_Hymalaya_Couleurs.png'
+import water_sahara from '../assets/img/shop/water/Bouteille_Sahara_Couleurs.png'
+import water_yosemite from '../assets/img/shop/water/bouteille_Yosemite.png'
+import CO2_amazonie from '../assets/img/shop/CO2/CO2_Amazonie.png'
+import CO2_ecolo from '../assets/img/shop/CO2/CO2_Ecolo.png'
+import CO2_Hymalaya from '../assets/img/shop/CO2/CO2_Hymalaya.png'
+import CO2_sahara from '../assets/img/shop/CO2/CO2_Sahara.png'
+import CO2_yosemite from '../assets/img/shop/CO2/CO2_Yosemite.png'
+
+const data = [
+    {
+        title: "Eau",
+        products: [
+            {
+                src: water_amazonie,
+                alt: 'Eau d\'Amazonie',
+                card: {
+                    title: "Eau d'Amazonie",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: water_ecolo,
+                alt: 'Eau écologique',
+                card: {
+                    title: "Eau écologique",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: water_Hymalaya,
+                alt: 'Eau d\'Hymalaya',
+                card: {
+                    title: "Eau d\'Hymalaya",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: water_sahara,
+                alt: 'Eau du Sahara',
+                card: {
+                    title: "Eau du Sahara",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: water_yosemite,
+                alt: 'Eau de yosemite',
+                card: {
+                    title: "Eau de yosemite",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            }
+        ],
+    },
+    {
+        title: "CO2",
+        products: [
+            {
+                src: CO2_amazonie,
+                alt: 'CO2 d\'Amazonie',
+                card: {
+                    title: "CO2 d'Amazonie",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: CO2_ecolo,
+                alt: 'CO2 écologique',
+                card: {
+                    title: "CO2 écologique",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: CO2_Hymalaya,
+                alt: 'CO2 d\'Hymalaya',
+                card: {
+                    title: "CO2 d\'Hymalaya",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: CO2_sahara,
+                alt: 'CO2 du Sahara',
+                card: {
+                    title: "CO2 du Sahara",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            },
+            {
+                src: CO2_yosemite,
+                alt: 'CO2 de yosemite',
+                card: {
+                    title: "CO2 de yosemite",
+                    subtitle: "France",
+                    localisation: localisation,
+                    content: [
+                        {
+                            text: "Ce coffret regroupe tous les ingrédients\n" +
+                                "          nécessaires pour faire pousser vos plantes\n" +
+                                "          européennes. Ce pack vient de matière 100%\n" +
+                                "          recyclés d'usine et de centrale. Contribuons\n" +
+                                "          ensemble à réduire l'effet de serre."
+                        },
+                        {
+                            text: "Super pour faire pousser :\n" +
+                                "          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,\n" +
+                                "          Tomate, Avocat, Tomate, Avocat,..."
+                        }
+                    ],
+                    labels: [
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        },
+                        {
+                            src: label
+                        }
+                    ]
+                },
+            }
+        ],
+    },
+]
+
+export default data
--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -1,72 +1,85 @@
-<script lang="ts">
+<script setup>
 import Header from '../components/Header/Header.vue';
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
+import sliderData from '../data/sliderData'
+import Glide from '@glidejs/glide'
+import '@glidejs/glide/dist/css/glide.core.min.css';
+import {onMounted, ref} from "vue";
 
-// Import Swiper styles
-import 'swiper/css';
+const activeRangeIndex = ref(0)
+// const activeCardIndex = ref(0)
+// const customData = ref(sliderData[0])
 
-export default {
-  components: {
-    Swiper,
-    SwiperSlide,
-    Header
-  },
-  setup() {
-    const onSwiper = (swiper) => {
-      console.log(swiper);
-    };
-    const onSlideChange = () => {
-      console.log('slide change');
-    };
-    return {
-      onSwiper,
-      onSlideChange,
-    };
-  },
-};
+onMounted(() => {
+  new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+  }).mount()
+})
+
 </script>
 <template>
-  <main>
+  <main class="h-screen flex flex-col overflow-hidden">
     <Header />
-    <ul class="font-title flex text-xl font-bold justify-center">
-      <li class="mx-4">Eau</li>
-      <li class="mx-4 opacity-50">CO2</li>
-      <li class="mx-4 opacity-50">Fertilisant</li>
-      <li class="mx-4 opacity-50">Uranium</li>
-      <li class="mx-4 opacity-50">Accessoire</li>
-      <li class="mx-4 opacity-50">Coffrets</li>
+    <ul class="font-title flex text-xl font-bold justify-center pb-10">
+      <li v-for="(range, index) in sliderData"
+          @click="activeRangeIndex = index; activeCardIndex = 0"
+          class="mx-4 opacity-50"
+          :class="{'opacity-100': activeRangeIndex === index}">{{ range.title }}</li>
     </ul>
-    <section class="flex w-full my-10">
-      <swiper
-          :slides-per-view="3"
-          :space-between="50"
-          :autoHeight="true"
-          @swiper="onSwiper"
-          @slideChange="onSlideChange"
-      >
-        <swiper-slide><img src="../assets/img/shop/co2/bouteille_amazonie.png" alt=""></swiper-slide>
-        <swiper-slide><img src="../assets/img/shop/co2/bouteille_ecolo.png" alt=""></swiper-slide>
-        <swiper-slide><img src="../assets/img/shop/co2/Bouteille_Hymalaya_Couleurs.png" alt=""></swiper-slide>
-        <swiper-slide><img src="../assets/img/shop/co2/Bouteille_Sahara_Couleurs.png" alt=""></swiper-slide>
-        <swiper-slide><img src="../assets/img/shop/co2/bouteille_yosemite.png" alt=""></swiper-slide>
-      </swiper>
-      <div class="border border-white rounded-lg py-4 px-6 w-2/5">
-        <h2>Fertilisant d'Amazonie</h2>
-        <p>France</p>
-        <p>Ce coffret regroupe tous les ingrédients
-          nécessaires pour faire pousser vos plantes
-          européennes. Ce pack vient de matière 100%
-          recyclés d'usine et de centrale. Contribuons
-          ensemble à réduire l'effet de serre.</p>
-        <p>Super pour faire pousser :
-          Tomate, Avocat, Tomate, Avocat, Tomate, Avocat,
-          Tomate, Avocat, Tomate, Avocat,...</p>
-      </div>
-    </section>
+
+    <div class="flex-1 flex">
+      <section class="flex w-full h-full">
+        <div class="w-3/5 h-full">
+          <div class="glide h-full">
+            <div class="glide__track h-3/4" data-glide-el="track">
+              <ul class="glide__slides h-full">
+                <li class="glide__slide w-full" v-for="product in sliderData[activeRangeIndex].products">
+                  <img class="h-full w-full" :src="product.src" :alt="product.alt">
+                </li>
+              </ul>
+            </div>
+
+            <!--          <div class="glide__bullets flex" data-glide-el="controls[nav]">-->
+            <!--            <button class="glide__bullet" data-glide-dir="=0" v-for="(product, index) in sliderData[activeRangeIndex].products"></button>-->
+            <!--          </div>-->
+            <div class="glide__bullets" data-glide-el="controls[nav]">
+              <button class="glide__bullet" data-glide-dir="=0"></button>
+              <button class="glide__bullet" data-glide-dir="=1"></button>
+              <button class="glide__bullet" data-glide-dir="=2"></button>
+            </div>
+          </div>
+        </div>
+
+
+<!--        <div class="w-2/5">-->
+<!--          <div v-for="(product, index) in sliderData[activeRangeIndex].products">-->
+<!--            <div class="border border-white rounded-lg py-4 px-6" v-if="index === activeCardIndex">-->
+<!--              <h2>{{ product.card.title }}</h2>-->
+<!--              <p>{{ product.card.subtitle }}</p>-->
+<!--              <img :src="product.card.localisation" alt="Localisation">-->
+<!--              <p v-for="text in product.card.content">{{ text.text }}</p>-->
+<!--              <img v-for="label in product.card.labels" :src="label.src" alt="Label">-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+
+        <div class="w-2/5">
+          <div class="border border-white rounded-3xl py-12 px-14 relative">
+            <h2 class="font-title text-4xl font-bold mb-3">{{ sliderData[0].products[0].card.title }}</h2>
+            <p class="font-bold mb-8">{{ sliderData[0].products[0].card.subtitle }}</p>
+            <img class="absolute top-20 right-16" :src="sliderData[0].products[0].card.localisation" alt="Localisation">
+            <p class="mb-8" v-for="text in sliderData[0].products[0].card.content">{{ text.text }}</p>
+            <ul class="flex justify-between mx-auto w-3/4">
+              <img v-for="label in sliderData[0].products[0].card.labels" :src="label.src" alt="Label">
+            </ul>
+          </div>
+        </div>
+      </section>
+    </div>
   </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
