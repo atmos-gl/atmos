9268d7d2031dadfb2f175bca5c1790fe7bb3071d
Johann <johanndumoulin734@gmail.com>
2022-06-08 19:40:27 +0200

    Add new text to section hp + minor fix

--- src/data/hpSectionsData.ts
+++ src/data/hpSectionsData.ts
@@ -1,27 +1,56 @@
-import section4Chrome from '../../public/assets/video/section-4-vp9-chrome.webm'
-import section4Safari from '../../public/assets/video/section-4-hevc-safari.mp4'
 const data = [
     {
         title: "Le monde entier dans votre assiette",
-        text: "Vous en avez marre de manger toujours les mêmes choses ? Vous rêvez d’essayer les fruits et légumes du monde entier mais vous ne les trouvez jamais en magasins ? Grâce à la serre Atmos faites pousser chez vous les fruits et légumes d’ailleurs. Transformer l’exotique en local, en réduisant l’impact de l’importation des fruits et légumes.",
+        texts: [
+            "Vous en avez marre de manger toujours les mêmes choses ?",
+            "Grâce à la serre Atmos :",
+            [
+                "Faites pousser chez vous les fruits et légumes d’ailleurs.",
+                "Transformer l’exotique en local",
+                "Réduiser l’impact de l’importation des fruits et légumes.",
+            ]
+        ],
         isReversed: false,
         video: 'section-1',
     },
     {
         title: "Manger végan, sain et équilibré",
-        text: "Vous êtes attachés à la cause animale ou vous voulez simplement réduire votre consommation de viande ? Passez à la serre. Notre marque est 100% cruelty Free et tous nos produits sont végan. Grâce à la serre Atome il vous sera beaucoup facile de manger 5 fruits et légumes par jour. Vous développerez de meilleures habitudes alimentaires.",
+        texts: [
+            "Vous êtes attachés à la cause animale ou vous voulez simplement réduire votre consommation de viande ?",
+            "Notre marque :",
+            [
+                "Est 100% cruelty Free et tous nos produits sont végan.",
+                "Facilite la consommationr 5 fruits et légumes par jour.",
+                "Vous aide à développer de meilleures habitudes alimentaires.",
+            ]
+        ],
         isReversed: true,
         video: 'section-2',
     },
     {
         title: "Faites tout pousser en un clin d’œil",
-        text: "Grâce à notre technologie à base de nucléaires, les plantes bénéficient d’un bienfait révolutionnaire qui permet de les faire pousser 100 plus rapidement qu’une plantation classique. Et tout cela dans le respect des traditions de leurs pays d’origine. Nous avons développé toute une gamme de produits venant du monde entier afin de vous permettre de développer tout type de fruits et légumes.",
+        texts: [
+            "Grâce à notre technologie à base de nucléaires :",
+            [
+                "Les plantes bénéficient d’un bienfait révolutionnaire",
+                "de les faire pousser 100 plus rapidement",
+                "Respecter les traditions de leurs pays d’origine",
+            ],
+            "Nous avons développé toute une gamme de produits venant du monde entier afin de vous permettre de développer tout type de fruits et légumes."
+        ],
         isReversed: false,
         video: 'section-3',
     },
     {
         title: "Autonome et connecté",
-        text: "Peur de la facture énergétique ? Plus besoin d’électricité pour alimenter votre serre. La serre est alimentée par un système à fission nucléaire grâce à une microscopique pilule d’uranium. Et cela pour plusieurs centaines d’années, il s’agit d’une énergie durable qui pourrait profiter à votre famille sur plusieurs décennies. De plus, vous pouvez agir sur votre serre à n’importe quel moment grâce à l’application connectée à celle-ci.",
+        texts: [
+            "Peur de la facture énergétique ?",
+            [
+                "Plus besoin d’électricité pour alimenter votre serre.",
+                "La serre est alimentée par un système à fission nucléaire grâce à une microscopique pilule d’uranium.",
+            ],
+            "Et cela pour plusieurs centaines d’années, il s’agit d’une énergie durable qui pourrait profiter à votre famille sur plusieurs décennies. De plus, vous pouvez agir sur votre serre à n’importe quel moment grâce à l’application connectée à celle-ci."
+        ],
         isReversed: true,
         video: 'section-4',
     },
