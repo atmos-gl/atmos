7824aa3ad82185377d0066d070361ff18c5a557c
Léo Boyer <contact@leoboyer.fr>
2022-06-08 10:15:27 +0200

    music

--- src/composables/useSoundEffect.ts
+++ src/composables/useSoundEffect.ts
@@ -22,3 +22,18 @@ export default function useSoundEffect(name: string) {
     }
     return { play }
 }
+
+export function initMusic() {
+    const music = new Howl({
+        src: ['/assets/audio/bg.mp3'],
+        loop: true
+    })
+    const startMusic = () => {
+        music.fade(0, 0.3, 5000)
+        music.play()
+        document.body.removeEventListener('scroll', startMusic)
+        document.body.removeEventListener('click', startMusic)
+    }
+    document.body.addEventListener('scroll', startMusic, {once: true})
+    document.body.addEventListener('click', startMusic, {once: true})
+}
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import './assets/css/global.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
-import useSoundEffect, {initSoundEffects} from './composables/useSoundEffect';
+import useSoundEffect, {initMusic, initSoundEffects} from './composables/useSoundEffect';
 
 createApp(App)
     .use(router)
     .mount('#app')
 
 initSoundEffects()
+initMusic()
