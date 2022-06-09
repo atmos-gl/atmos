36db1014e0cb6a0a44b6bccb1de01f59a606f443
Léo Boyer <contact@leoboyer.fr>
2022-06-09 10:44:18 +0200

    Fixes homepage

--- src/components/Header/HeroHeader.vue
+++ src/components/Header/HeroHeader.vue
@@ -13,13 +13,13 @@ const scrollingLines = 4
       <HeaderScene/>
       <img :src="bottlesImage" class="absolute left-1/2 transform -translate-x-1/2 bottom-1/6 h-1/4 pointer-events-none">
     </div>
-    <div class="o-container flex items-center text-jade flex-1 pt-0 pb-0 gap-4">
+    <div class="o-container flex items-center text-jade flex-1 pt-0 pb-0 gap-4 relative pointer-events-none">
       <div
           class="absolute-y-center text-customBig font-title font-bold uppercase leading-none flex flex-col opacity-15 -z-1">
         <ScrollingText v-for="line in scrollingLines" :key="line"/>
       </div>
 
-      <div class="w-2/5 mb-12">
+      <div class="w-2/5 mb-12 pointer-events-auto">
         <h2 class="font-title font-bold text-8xl">Atmos</h2>
         <h3 class="my-6 text-4xl">La serre qui réduit l'effet de serre</h3>
         <p>Atmos, votre serre connectée vous facilite le quotidien en adoptant une démarche écologgique et durable.
